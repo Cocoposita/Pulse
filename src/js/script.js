f1b6c14583742ef306bd1e9b3fa9094f389d268b
Julia <J.Naizy@gmail.com>
2021-08-16 22:15:08 +0300

    второй слайдер

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         //adaptiveHeigth: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="/icons/left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="/icons/right.png"></button>',
+//         responsive: [
+//             {
+//             breakpoint: 992,
+//             settings: {
+//                 dots: true,
+//                 arrows: false
+//             }
+//         }
+//         ]
+//     }
+//     ); 
+//   });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 3,
+    slideBy: 'page',
+    autoplay: false
+  });
+
+  document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+  };
